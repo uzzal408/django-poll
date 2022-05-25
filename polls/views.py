1bd6ed7d9d400d4d214f963e0b6df124adb3a614
uzzal408 <imail2226@diu.edu.bd>
2022-05-26 00:16:05 +0600

    added tamplate in view and get data from db

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world. Your in in polls app')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html',context)
 
 def details(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request,question_id):
     return HttpResponse("You're looking at the results of %s." % question_id)
